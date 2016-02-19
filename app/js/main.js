ff38e6dd0c67b5308abaaa65f2fad822f582a30a
Alex Guevara <aguevara@matoot.com>
2016-02-19 00:02:29 -0800

    Animate monkey greyscale to color

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,17 @@
 $( function( ) {
     'use strict';
+    
+    var color = {gray:0};
+    var image = $("#wanted-monkey")[0];
+    
+    
+    TweenMax.to(color, 2, {gray:1, onUpdate:applyColor, onUpdateParams:[image], repeat:50, yoyo:true, repeatDelay:0.2})
+    
+    function applyColor(element) {
+        //apply filter yourself
+        element.style["-webkit-filter"] = "grayscale(" + color.gray + ")"
+    }
+
     $.fn.serializeFormJSON = function () {
 
         var o = {};
@@ -26,41 +38,52 @@ $( function( ) {
             this.contactForm( );
         },
 
+        sendPost: function ( object ) {
+            console.log( 'entro' );
+            var currentForm = $( object ).closest( 'form.matoot-form' );
+
+            var emailElem = currentForm.find( "[name='email']" ).first( );
+            var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+
+            currentForm.find( ".matoot-form-result" ).removeClass( 'active' ).html( "" );
+            if ( !emailReg.test( emailElem.val( ) ) || '' === emailElem.val( ) ) {
+                currentForm.find(".matoot-form-result").addClass('active').html("Invalid Email, please provide a correct email!");
+                emailElem.focus( );
+            } else {
+                var data = $( currentForm ).serializeFormJSON( );
+                $.ajax ({
+                    url: 'http://api.matoot.com/newWaitingList',
+                    type: "POST",
+                    data: JSON.stringify( data ),
+                    dataType: "json",
+                    contentType: "application/json; charset=utf-8",
+                    success: function( ){
+                        currentForm.find( '.matoot-form-result' ).addClass( 'active' ).html( "Congratulations!, you are now on the waiting list." );
+                        currentForm.resetForm( );
+                    },
+                    error: function ( request, status, error ) {
+                        currentForm.find( ".matoot-form-result" ).addClass( 'active' ).html( request.responseJSON.msg );
+                    }
+                });
+            }
+        },
+
         contactForm: function( ) {
 
             $.fn.resetForm = function ( ) {
               $( this ).each (function( ) { this.reset( ); } );
             }
 
-            $('.matoot-form-button').click( function( ){
-                var currentForm = $( this ).closest( 'form.matoot-form' );
+            $( '#email_input' ).keypress( function ( e ) {
+              if ( e.which == 13 ) {
+                Main.sendPost( this );
+                return false;    //<---- Add this line
+              }
+            });
 
-                var emailElem = currentForm.find( "[name='email']" ).first( );
-                var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-
-                currentForm.find( ".matoot-form-result" ).removeClass( 'active' ).html( "" );
-                if ( !emailReg.test( emailElem.val( ) ) || '' === emailElem.val( ) ) {
-                    currentForm.find(".matoot-form-result").addClass('active').html("Invalid Email, please provide a correct email!");
-                    emailElem.focus( );
-                } else {
-                    var data = $( currentForm ).serializeFormJSON( );
-                    $.ajax ({
-                        url: 'http://api.matoot.com/waitingList',
-                        type: "POST",
-                        data: JSON.stringify( data ),
-                        dataType: "json",
-                        contentType: "application/json; charset=utf-8",
-                        success: function( ){
-                            currentForm.find(".matoot-form-result").addClass('active').html( "Congratulations!, you are now on the waiting list." );
-                            currentForm.resetForm( );
-                        },
-                        error: function ( request, status, error ) {
-                            currentForm.find(".matoot-form-result").addClass('active').html( request.responseText );
-                        }
-                    });
-                }
+            $( '.matoot-form-button' ).click( function( ){
+                Main.sendPost( this );
                 return false;
-
             });
 
         }
